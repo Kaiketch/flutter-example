273950792a0cda20ea86cc9a43ea1c2d096de5f7
Kaiketch <akaike@kumofune.jp>
2022-02-27 19:36:17 +0900

    add history page

---
+++ lib/ui/history/history_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_example/ui/component/loading_view.dart';
+import 'package:flutter_example/ui/history/history_viewmodel.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HistoryPage extends HookConsumerWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final historyViewModel = ref.read(historyStateProvider.notifier);
+    final historyState = ref.watch(historyStateProvider);
+    final shouldReload =
+        ref.watch(historyStateProvider.select((value) => value.shouldReload));
+
+    useEffect(() {
+      Future(() {
+        historyViewModel.getKeywords();
+      });
+      return null;
+      // 初期表示またはリロードフラグが立っている場合に発火させる
+    }, [shouldReload]);
+
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    final exception = historyState.exception;
+    if (exception != null) {
+      Future(() {
+        debugPrint("exception ${exception.toString()}");
+        const snackBar = SnackBar(content: Text('エラーが発生しました'));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      });
+    }
+
+    return historyState.isLoading
+        ? const LoadingView()
+        : ListView.builder(
+            itemCount: historyState.keywordList?.length ?? 0,
+            itemBuilder: (context, index) {
+              return ListTile(
+                  title:
+                      Text("tile ${historyState.keywordList?[index].keyword}"));
+            });
+  }
+}
