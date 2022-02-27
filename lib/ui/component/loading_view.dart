c0f6364eef7faf4a5e72cb7097aecc97264d2baa
Kaiketch <akaike@kumofune.jp>
2022-02-27 19:36:51 +0900

    add loading

---
+++ lib/ui/component/loading_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+      ),
+    );
+  }
+}
