a50b504224692eb84f9af89bdea51aff8c79e53b
Kaiketch <akaike@kumofune.jp>
2022-02-27 19:35:37 +0900

    add event list page

---
+++ lib/ui/event/event_list_page.dart
@@ -0,0 +1,60 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_example/ui/component/loading_view.dart';
+import 'package:flutter_example/ui/event/event_list_viewmodel.dart';
+import 'package:flutter_example/ui/history/history_viewmodel.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class EventListPage extends HookConsumerWidget {
+  const EventListPage(this._keyword, {Key? key}) : super(key: key);
+
+  final String _keyword;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final eventListViewModel = ref.read(eventListStateProvider.notifier);
+    final eventListState = ref.watch(eventListStateProvider);
+    final historyViewModel = ref.read(historyStateProvider.notifier);
+
+    useEffect(() {
+      Future(() {
+        eventListViewModel.getEvents(_keyword);
+        // historyを更新するため。もっと良い方法ありそう
+        historyViewModel.setShouldReload(true);
+      });
+      return null;
+    }, const []);
+
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    final exception = eventListState.exception;
+    if (exception != null) {
+      Future(() {
+        const snackBar = SnackBar(content: Text('エラーが発生しました'));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      });
+    }
+
+    final result = eventListState.eventResult;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Keyword: $_keyword"),
+        leading: BackButton(
+          onPressed: () {
+            AutoRouter.of(context).pop();
+          },
+        ),
+      ),
+      body: eventListState.isLoading
+          ? const LoadingView()
+          : ListView.builder(
+              itemCount: result?.results_returned ?? 0,
+              itemBuilder: (context, index) {
+                return ListTile(
+                    title: Text("tile ${result?.events[index].title}"));
+              },
+            ),
+    );
+  }
+}
