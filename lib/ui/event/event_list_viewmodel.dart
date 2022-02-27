61bc9bffd48e63972884507198e092e57ba55a4a
Kaiketch <akaike@kumofune.jp>
2022-02-27 19:36:07 +0900

    add event list viewmdel

---
+++ lib/ui/event/event_list_viewmodel.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_example/data/repository/event_repository.dart';
+import 'package:flutter_example/data/repository/keyword_repository.dart';
+import 'package:flutter_example/ui/event/event_list_state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final eventListStateProvider =
+    StateNotifierProvider.autoDispose<EventListViewModel, EventListState>(
+  (ref) => EventListViewModel(
+    ref.read(eventRepositoryProvider),
+    ref.read(keywordRepositoryProvider),
+  ),
+);
+
+class EventListViewModel extends StateNotifier<EventListState> {
+  EventListViewModel(this._eventRepository, this._keywordRepository)
+      : super(EventListState());
+
+  final EventRepository _eventRepository;
+  final KeywordRepository _keywordRepository;
+
+  Future<void> getEvents(String keyword) async {
+    state = state.copyWith(isLoading: true);
+    _keywordRepository.setKeyword(keyword);
+
+    _eventRepository
+        .getEvents(keyword)
+        .catchError(
+          (e) {
+            state = state.copyWith(exception: e);
+          },
+        )
+        .then(
+          (value) => {
+            state = state.copyWith(eventResult: value),
+          },
+        )
+        .whenComplete(
+          () {
+            state = state.copyWith(isLoading: false);
+          },
+        );
+  }
+}
