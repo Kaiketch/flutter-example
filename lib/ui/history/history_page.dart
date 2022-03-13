049c4d67226c624dc63b9c7838a479ecca448437
Kaiketch <akaike@kumofune.jp>
2022-03-13 14:06:05 +0900

    add history tap event

--- lib/ui/history/history_page.dart
+++ lib/ui/history/history_page.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_example/app_router.dart';
 import 'package:flutter_example/ui/component/loading_view.dart';
 import 'package:flutter_example/ui/event/event_list_viewmodel.dart';
 import 'package:flutter_example/ui/history/history_viewmodel.dart';
@@ -13,16 +15,16 @@ class HistoryPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final historyViewModel = ref.read(historyStateProvider.notifier);
     final historyState = ref.watch(historyStateProvider);
-    final searchedKeyword =
-        ref.watch(eventListStateProvider.select((value) => value.keyword));
+    final eventResult =
+        ref.watch(eventListStateProvider.select((value) => value.eventResult));
 
     useEffect(() {
       Future(() {
         historyViewModel.onUpdateHistory();
       });
-      return historyViewModel.dispose;
+      return null;
       // 初期表示またはキーワド検索が行われた場合に発火させる
-    }, [searchedKeyword]);
+    }, [eventResult]);
 
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
     final exception = historyState.exception;
@@ -38,8 +40,18 @@ class HistoryPage extends HookConsumerWidget {
         : ListView.builder(
             itemCount: historyState.keywordList?.length ?? 0,
             itemBuilder: (context, index) {
+              final keyword = historyState.keywordList?[index].keyword;
+
               return ListTile(
-                  title: Text("${historyState.keywordList?[index].keyword}"));
+                title: Text(keyword ?? ""),
+                onTap: () {
+                  if (keyword != null) {
+                    AutoRouter.of(context).push(
+                      EventListRoute(keyword: keyword),
+                    );
+                  }
+                },
+              );
             },
           );
   }
